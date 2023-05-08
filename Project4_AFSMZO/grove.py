dab32cb5b72cbd44b5b5b39da9891b603d83c0a5
ZackOBrien <obrienzj19@gcc.edu>
2023-05-08 17:27:39 -0400

    implemnted same main from example.

--- Project4_AFSMZO/grove.py
+++ Project4_AFSMZO/grove.py
@@ -3,4 +3,18 @@ from grove_lang import *
 
 if __name__ == "__main__":
     # TODO: Implement your REPL here
-	pass
+    print("Welcome to the Grove REPL!")
+    print("Enter your commands or ':done' to exit")
+    while True:
+        s: str = input('> ')
+        if s.strip() == ':done': break
+        try:
+            x = Command.parse(s).eval()
+            if x is not None: print(x)
+        except GroveParseError as e:
+            print(f"Error parsing {s}")
+            print(e)
+        except GroveEvalError as e:
+            print(f"Error Evaluating {s}")
+            print(e)
+    print("Goodbye and thank you for using Grove!")
